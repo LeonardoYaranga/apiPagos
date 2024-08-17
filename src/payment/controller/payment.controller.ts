301424b3c54765585950e857ef4511f77f99c274
LeonardoYaranga <ljyaranga@espe.edu.ec>
2024-08-17 17:00:02 -0500

    getOne, update,delete

--- src/payment/controller/payment.controller.ts
+++ src/payment/controller/payment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards,Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards,Request, Param, Put, Delete} from '@nestjs/common';
 import { PaymentService } from '../service/payment.service';
 import { PaymentGuard } from '../guard/payment.guard';
 
@@ -15,7 +15,30 @@ export class PaymentController {
 
     @Get('getData')
     @UseGuards(PaymentGuard)
-    async getData(): Promise<any> {
-        return await this.paymentService.getPayments();
+    async getData(@Request() request:any): Promise<any> {
+        const userId= request.user.sub;  
+        return await this.paymentService.getPayments(userId);
     }
+
+    @Get('getDataById/:id')
+    @UseGuards(PaymentGuard)
+    async getDataById(@Param('id') id: string, @Request() request: any): Promise<any> {
+        const userId = request.user.sub;
+        return await this.paymentService.getPaymentById(userId, id);
+    }
+
+    @Put('updateData/:id')
+    @UseGuards(PaymentGuard)
+    async updateData(@Body() data: any,@Request() request:any,@Param('id') id: string): Promise<void> {
+        const userId= request.user.sub;  
+        await this.paymentService.updatePayment({...data,userId},id);
+    }
+
+    @Delete('deleteData/:id')
+    @UseGuards(PaymentGuard)
+    async deleteData(@Body() data: any,@Request() request:any,@Param('id') id: string): Promise<void> {
+        const userId= request.user.sub;  
+        await this.paymentService.deletePayment({...data,userId},id);
+    }
+
 }
--- src/payment/service/payment.service.ts
+++ src/payment/service/payment.service.ts
@@ -4,25 +4,64 @@ import { firebaseDataBase } from 'src/firebaseConfig';
 
 @Injectable()
 export class PaymentService {
-    async createPayment(data: any): Promise<void> {
-        const dataRef = ref(firebaseDataBase, 'Payment');
-        const newElementRef = push(dataRef, {dataRef: data});
-        await set(newElementRef, {...data, userId: data.userId});  //aqui capaz se inserta un objeto con 2 userId ahi borrarle el userId extra
+  async createPayment(data: any): Promise<void> {
+    const dataRef = ref(firebaseDataBase, 'Payment');
+    const newElementRef = push(dataRef, { dataRef: data });
+    await set(newElementRef, { ...data, userId: data.userId }); 
+  }
+  //Aqui debe ser que solo sean los pagos de ese usuario que tiene el token y por ende tiene el userId
+  async getPayments(userId: string): Promise<any> {
+    const dataRef = ref(firebaseDataBase, 'Payment');
+    const snapshot: DataSnapshot = await get(dataRef);
+    const payments = snapshot.val();
+    const userPayments = Object.values(payments).filter(
+      (payment: any) => payment.userId === userId,
+    );
+    return userPayments;
+  }
+
+  //Aqui se debe hacer un get de un solo pago por el id del pago y el userId
+
+  async getPaymentById(userId: string, paymentId: string): Promise<any> {
+    const dataRef = ref(firebaseDataBase, `Payment/${paymentId}`);
+    const snapshot: DataSnapshot = await get(dataRef);
+    const payment = snapshot.val();
+
+    // Verifica que el pago pertenece al usuario autenticado
+    if (payment && payment.userId === userId) {
+      return payment;
+    } else {
+      throw new Error('Payment not found.');
     }
- //Aqui debe ser que solo sean los pagos de ese usuario que tiene el token y por ende tiene el userId
-    async getPayments(): Promise<any> {  
-        const dataRef = ref(firebaseDataBase, 'Payment');
-        const snapshot: DataSnapshot = await get(dataRef);
-        if (snapshot.exists()) {
-            return snapshot.val();
+  }
+
+
+  //Aqui se debe hacer un update de un solo pago por el id del pago y el userId
+  async updatePayment(data: any,paymentId: string): Promise<void> {
+    const paymentRef = ref(firebaseDataBase, `Payment/${paymentId}`);
+    const snapshot: DataSnapshot = await get(paymentRef);
+    const payment = snapshot.val();
+
+    // Verifica si el pago existe y si pertenece al usuario
+    if (payment && payment.userId === data.userId) {
+      await set(paymentRef, { ...payment, ...data });
+    } else {
+      throw new Error('Payment not found.');
+    }
+  }
+
+  //Aqui se debe hacer un delete de un solo pago por el id del pago y el userId
+    async deletePayment(data: any,paymentId): Promise<void> {
+        const paymentRef = ref(firebaseDataBase, `Payment/${paymentId}`);
+        const snapshot: DataSnapshot = await get(paymentRef);
+        const payment = snapshot.val();
+    
+        // Verifica si el pago existe y si pertenece al usuario
+        if (payment && payment.userId === data.userId) {
+        await set(paymentRef, null);
         } else {
-            return null;
+        throw new Error('Payment not found.');
         }
     }
-
-//Aqui se debe hacer un get de un solo pago por el id del pago y el userId
-//Aqui se debe hacer un update de un solo pago por el id del pago y el userId
-//Aqui se debe hacer un delete de un solo pago por el id del pago y el userId
-
 
 }
